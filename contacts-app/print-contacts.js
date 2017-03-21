2cfb5b8adb91f0cbf265771ea23b390ef5f74079
JulParman <julian.parman@gmail.com>
2017-03-21 12:50:54 +0200

    Encapsulation

---
+++ contacts-app/print-contacts.js
@@ -0,0 +1,24 @@
+contactsApp.printContacts = (function () {
+
+    function getContactFromArray() {
+        var data = contactsApp.contactLocalStorage.getDataFromLocalStorage();
+        var newRow = '';
+        var nro = 0;
+        for (var i = 0; i < data.length; i++) {
+            newRow += "<tr id='" + nro + "'><td>" + data[i].firstName + "</td>" +
+                "<td>" + data[i].lastName + "</td>" +
+                "<td>" + data[i].phone + "</td>" +
+                "<td>" + "<a target='_blank' href='https://www.google.com/maps/place/" + data[i].address + "'>" + data[i].address + "</a>" + "</td></tr>";
+            nro++;
+        }
+        var tableElement = document.getElementById('utable');
+        tableElement.innerHTML = newRow;
+
+    }
+
+    return {
+        getContacts: function () {
+            getContactFromArray();
+        }
+    }
+})();
