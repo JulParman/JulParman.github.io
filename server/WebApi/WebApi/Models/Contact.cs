f15c279d1f51c7662c21ff8a06d62699826755be
JulParman <julian.parman@gmail.com>
2017-03-21 14:53:35 +0200

    Server folder and webapi added

---
+++ server/WebApi/WebApi/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Contact
+    {
+        public Contact(string firstName, string lastName, long phone, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Address = address;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
