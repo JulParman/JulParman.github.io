5e4444689cd676df284561ed46910c12ff09e040
JulParman <julian.parman@gmail.com>
2017-04-03 10:12:53 +0300

    Update and delete bug fixes

--- client/contacts-app/contact-server.js
+++ client/contacts-app/contact-server.js
@@ -19,13 +19,13 @@ contactsApp.getDataFromServer = (function () {
 
     function deleteContact() {
         try {
-            var selectedRow = document.querySelector(".is-checked").parentNode.parentNode;
-            var id = selectedRow.dataset.key;
+            var selectedRowToDelete = document.querySelector(".is-checked").parentNode.parentNode;
+            var idDelete = selectedRowToDelete.dataset.key;
             event.preventDefault();
-            if (id) {
+            if (idDelete) {
                 $.ajax({
                     type: "DELETE",
-                    url: "http://localhost:51057/api/contact/" + id,
+                    url: "http://localhost:51057/api/contact/" + idDelete,
                     contentType: "application/json; charset=utf-8",
                     dataType: "json",
                     timeout: 10000,
@@ -62,52 +62,59 @@ contactsApp.getDataFromServer = (function () {
             deleteContact();
         },
         updateToServer: function () {
-            var rowToUpdate = document.querySelector(".is-checked").parentNode.parentNode;
-            var id = rowToUpdate.dataset.key;
-            var arrayIndex = id - 1;
-            var obj;
+            try{
+                var rowToUpdate = document.querySelector(".is-checked").parentNode.parentNode;
+                var id = rowToUpdate.dataset.key;
+                var arrayIndex = id - 1;
+                var obj;
 
-            var firstName = document.getElementById('txtfname').value;
-            var lastName = document.getElementById('txtlname').value;
-            var phone = document.getElementById('txtphone').value;
-            var address = document.getElementById('txtaddress').value;
-            var city = document.getElementById('txtcity').value;
+                var firstName = document.getElementById('txtfname').value;
+                var lastName = document.getElementById('txtlname').value;
+                var phone = document.getElementById('txtphone').value;
+                var address = document.getElementById('txtaddress').value;
+                var city = document.getElementById('txtcity').value;
 
-            if (rowToUpdate) {
+                if (rowToUpdate) {
 
-                if (firstName) {
-                    contactsArray[arrayIndex].firstName = firstName;
-                }
-                if (lastName) {
-                    contactsArray[arrayIndex].lastName = lastName;
-                }
-                if (phone) {
-                    contactsArray[arrayIndex].phone = phone;
-                }
-                if (address) {
-                    contactsArray[arrayIndex].address = address;
-                }
-                if (city) {
-                    contactsArray[arrayIndex].city = city;
-                }
-                obj = JSON.stringify(contactsArray[arrayIndex]);
-                event.preventDefault();
-                $.ajax({
-                    url: "http://localhost:51057/api/contact/" + id,
-                    type: "PUT",
-                    data: obj,
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    timeout: 100000,
-                    success: function () {
-                        location.reload();
-                    },
-                    error: function () {
-                        alert("error");
+                    if (firstName) {
+                        contactsArray[arrayIndex].firstName = firstName;
                     }
-                });
+                    if (lastName) {
+                        contactsArray[arrayIndex].lastName = lastName;
+                    }
+                    if (phone) {
+                        contactsArray[arrayIndex].phone = phone;
+                    }
+                    if (address) {
+                        contactsArray[arrayIndex].address = address;
+                    }
+                    if (city) {
+                        contactsArray[arrayIndex].city = city;
+                    }
+                    obj = JSON.stringify(contactsArray[arrayIndex]);
+                    event.preventDefault();
+                    $.ajax({
+                        url: "http://localhost:51057/api/contact/" + id,
+                        type: "PUT",
+                        data: obj,
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        timeout: 100000,
+                        success: function () {
+                            location.reload();
+                        },
+                        error: function () {
+                            alert("error");
+                        }
+                    });
 
+                }
             }
+            catch (err)
+            {
+                alert("Error on update. " + err);
+            }
+
         }
 
     }
